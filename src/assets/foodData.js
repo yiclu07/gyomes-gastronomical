dcc149eb242dda2c925b9f2e950f04e19c3c9541
yiclu07 <yi.c.lu07@gmail.com>
2024-11-16 21:51:57 -0500

    Add mock food/menu data

---
+++ src/assets/foodData.js
@@ -0,0 +1,84 @@
+const foodData = [
+  {
+    id: 1,
+    name: "Mallorn-Wrapped Lembas",
+    price: 9.99,
+  },
+  {
+    id: 2,
+    name: "Eldraine's Wild Bounty",
+    price: 17.99,
+  },
+  {
+    id: 3,
+    name: "Lost Traveler's Respite",
+    price: 22.99,
+  },
+  {
+    id: 4,
+    name: "The Royal Entourage",
+    price: 49.99,
+  },
+  {
+    id: 5,
+    name: "Earthen Delight",
+    price: 24.99,
+  },
+  {
+    id: 6,
+    name: "Deathspot Pie",
+    price: 39.99,
+  },
+  {
+    id: 7,
+    name: "Maw-Cage Wellington",
+    price: 49.99,
+  },
+  {
+    id: 9,
+    name: "Cosmic Fiesta",
+    price: 17.99,
+  },
+  {
+    id: 10,
+    name: "The Unfinity-Stacked",
+    price: 34.99,
+  },
+  {
+    id: 11,
+    name: "Itinerant Celebration",
+    price: 49.99,
+  },
+  {
+    id: 12,
+    name: "Echoes of the Shire",
+    price: 19.99,
+  },
+  {
+    id: 13,
+    name: "Minas Tirith Party Platter",
+    price: 74.99,
+  },
+  {
+    id: 14,
+    name: "Ilharg's Raze-Bacon",
+    price: 69.99,
+  },
+  {
+    id: 17,
+    name: "Candied Maw Lava-Cake",
+    price: 37.99,
+  },
+  {
+    id: 18,
+    name: "Second Breakfast",
+    price: 19.99,
+  },
+  {
+    id: 19,
+    name: "Newt-Eye Gaze Soup",
+    price: 20.99,
+  },
+];
+
+export default foodData;
